e321dd0f6c6436eb467c8b92b2fd61b378408413
Aleksey Romanov <lex.at.net@gmail.com>
2016-10-22 22:50:35 +0600

    Add history configuration

--- init.sh
+++ init.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+
+
 apt-get install goldendict
 
 # setup tor-browser
 add-apt-repository ppa:webupd8team/tor-browser
 apt-get update
 apt-get install tor-browser
+
+sed -i "s/HISTCONTROL=/HISTCONTROL=ignoreboth:erasedups #HISTCONTROL=/g" ~/.bachrc
+sed -i "s/HISTSIZE=/HISTSIZE=10000 #HISTSIZE=/g" ~/.bachrc
+sed -i "s/HISTFILESIZE=/HISTFILESIZE=10000 #HISTFILESIZE=/g" ~/.bachrc
