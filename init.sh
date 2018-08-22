762f6c29985b6899ef441529e480e743ec969d89
lexatnet <lex.at.net@gmail.com>
2018-08-22 14:48:56 +0600

    update init script for Ubuntu 18.04 LTS

--- init.sh
+++ init.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-apt-get update
-apt-get install mc
-apt-get install chromium-browser
+apt update
+apt install git
+apt install mc
+apt install chromium-browser
 
 #install ssh server
-apt-get install openssh-server
-sudo service ssh status
+apt install openssh-server
+service ssh status
 
 # install archive utils
-sudo apt-get install unzip
-sudo apt-get install p7zip-full
+apt install unzip
+apt install p7zip-full
 
 #install goldendict
-apt-get install goldendict
+apt install goldendict
 # TODO: extract dictionaries
 
 # setup tor-browser
-add-apt-repository ppa:webupd8team/tor-browser
-apt-get update
-apt-get install tor-browser
+apt install torbrowser-launcher
 
 sed -i "s/HISTCONTROL=/HISTCONTROL=ignoreboth:erasedups #HISTCONTROL=/g" ~/.bashrc
 sed -i "s/HISTSIZE=/HISTSIZE=10000 #HISTSIZE=/g" ~/.bashrc
@@ -27,11 +26,11 @@ sed -i "s/HISTFILESIZE=/HISTFILESIZE=10000 #HISTFILESIZE=/g" ~/.bashrc
 
 #install keepassx
 sudo add-apt-repository ppa:eugenesan/ppa
-apt-get update
-sudo apt-get install keepassx
+apt update
+sudo apt install keepassx
 
 #install node
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 source ~/.bashrc
 nvm install node
 
@@ -47,14 +46,30 @@ rbenv global 2.5.0
 gem install bundler
 
 # install docker
-apt-get update
-apt-get install docker-ce
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+apt update
+apt install docker-ce
 groupadd docker
 usermod -aG docker $USER
 
 #install docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.20.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
 
 #install sqlite3
-apt-get install -y sqlite3
+apt install -y sqlite3
+
+#install VPN managers
+apt install openvpn network-manager-openvpn
+apt install openconnect network-manager-openconnect
