e7469140a3f4a30a13d236489aad0fb3ebf6e156
lexatnet <lex.at.net@gmail.com>
2018-08-28 23:33:27 +0600

    add virtualbox installation

--- init.sh
+++ init.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 
-apt-get update
-apt-get install mc
-apt-get install chromium-browser
+apt update
+apt install mc
+apt install chromium-browser
 
 #install ssh server
-apt-get install openssh-server
+apt install openssh-server
 sudo service ssh status
 
 # install archive utils
-sudo apt-get install unzip
-sudo apt-get install p7zip-full
+sudo apt install unzip
+sudo apt install p7zip-full
 
 #install goldendict
-apt-get install goldendict
+apt install goldendict
 # TODO: extract dictionaries
 
 # setup tor-browser
+apt install tor
 add-apt-repository ppa:webupd8team/tor-browser
-apt-get update
-apt-get install tor-browser
+apt update
+apt install tor-browser
 
 sed -i "s/HISTCONTROL=/HISTCONTROL=ignoreboth:erasedups #HISTCONTROL=/g" ~/.bashrc
 sed -i "s/HISTSIZE=/HISTSIZE=10000 #HISTSIZE=/g" ~/.bashrc
 sed -i "s/HISTFILESIZE=/HISTFILESIZE=10000 #HISTFILESIZE=/g" ~/.bashrc
+echo "stty -ixon" >> ~/.bashrc
 
 #install keepassx
 sudo add-apt-repository ppa:eugenesan/ppa
-apt-get update
-sudo apt-get install keepassx
+apt update
+sudo apt install keepassx
 
 #install node
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 source ~/.bashrc
 nvm install node
 
@@ -47,14 +49,14 @@ rbenv global 2.5.0
 gem install bundler
 
 # install docker
-apt-get update
-apt-get install docker-ce
+apt update
+apt install docker-ce
 groupadd docker
 usermod -aG docker $USER
 
 #install docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.20.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 #install sqlite3
-apt-get install -y sqlite3
+apt install -y sqlite3
